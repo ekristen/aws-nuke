722c540ca7cbb032719bc56fe09c69445e8073de
Erik Kristensen <erik@erikkristensen.com>
2024-01-16 16:53:57 -0700

    chore/docs: testing and mocks (#23)
    * chore: cleanup and standardize mock generation
    
    * chore: improving docs and makefile

---
+++ resources/cloudformation_mock_test.go
@@ -0,0 +1,4 @@
+//go:generate ../mocks/generate_mocks.sh cloudformation cloudformationiface
+package resources
+
+// Note: empty on purpose, this file exist purely to generate mocks for the CloudFormation service
