0b654cf24a90c00002f7e1bc25f507bed94e64f6
Erik Kristensen <erik@erikkristensen.com>
2024-01-25 16:46:09 -0700

    migrate apprunner resources from upstream to libnuke format (#38)
    * Adding support for AppRunner services  (#1060)
    
    * adding code for removing custom origin request policies
    
    * rename resource
    
    * test for replication
    
    * revert secretsmanager change
    
    * undo this
    
    * revert mod and sum changes
    
    * add resources for redshift scheduled actions
    
    * remove cloudfront resource
    
    * clean up
    
    * cloudwatch rum app
    
    * first attempt at apprunner-service
    
    * first attempt at apprunner-service
    
    * add apprunner connection as well
    
    * clean up
    
    * chore: migrate resources to libnuke format
    
    ---------
    
    Co-authored-by: Mike Schouw <49021968+MikeSchouw@users.noreply.github.com>

---
+++ resources/apprunner-service.go
@@ -0,0 +1,80 @@
+package resources
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/service/apprunner"
+
+	"github.com/ekristen/libnuke/pkg/resource"
+	"github.com/ekristen/libnuke/pkg/types"
+
+	"github.com/ekristen/aws-nuke/pkg/nuke"
+)
+
+type AppRunnerService struct {
+	svc         *apprunner.AppRunner
+	ServiceArn  *string
+	ServiceId   *string
+	ServiceName *string
+}
+
+const AppRunnerServiceResource = "AppRunnerService"
+
+func init() {
+	resource.Register(&resource.Registration{
+		Name:   AppRunnerServiceResource,
+		Scope:  nuke.Account,
+		Lister: &AppRunnerServiceLister{},
+	})
+}
+
+type AppRunnerServiceLister struct{}
+
+func (l *AppRunnerServiceLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
+	opts := o.(*nuke.ListerOpts)
+
+	svc := apprunner.New(opts.Session)
+	resources := make([]resource.Resource, 0)
+
+	params := &apprunner.ListServicesInput{}
+
+	for {
+		resp, err := svc.ListServices(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, item := range resp.ServiceSummaryList {
+			resources = append(resources, &AppRunnerService{
+				svc:         svc,
+				ServiceArn:  item.ServiceArn,
+				ServiceId:   item.ServiceId,
+				ServiceName: item.ServiceName,
+			})
+		}
+
+		if resp.NextToken == nil {
+			break
+		}
+
+		params.NextToken = resp.NextToken
+	}
+
+	return resources, nil
+}
+
+func (f *AppRunnerService) Remove(_ context.Context) error {
+	_, err := f.svc.DeleteService(&apprunner.DeleteServiceInput{
+		ServiceArn: f.ServiceArn,
+	})
+
+	return err
+}
+
+func (f *AppRunnerService) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("ServiceArn", f.ServiceArn)
+	properties.Set("ServiceId", f.ServiceId)
+	properties.Set("ServiceName", f.ServiceName)
+	return properties
+}
