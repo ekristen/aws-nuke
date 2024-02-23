549eec4eabb024b45f7dc5d080886c0ce1143e8d
Erik Kristensen <erik@erikkristensen.com>
2024-02-22 19:10:09 -0700

    tests: update mocks for new aws-sdk version

--- resources/cloudformation_mock_test.go
+++ resources/cloudformation_mock_test.go
@@ -1,4 +1,6 @@
 //go:generate ../mocks/generate_mocks.sh cloudformation cloudformationiface
 package resources
 
+import _ "github.com/golang/mock/mockgen"
+
 // Note: empty on purpose, this file exist purely to generate mocks for the CloudFormation service
--- resources/iam_mock_test.go
+++ resources/iam_mock_test.go
@@ -1,4 +1,6 @@
 //go:generate ../mocks/generate_mocks.sh iam iamiface
 package resources
 
+import _ "github.com/golang/mock/mockgen"
+
 // Note: empty on purpose, this file exist purely to generate mocks for the IAM service
