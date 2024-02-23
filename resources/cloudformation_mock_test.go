b67d8392b133139d22c28be7a5177285eced634c
Erik Kristensen <erik@erikkristensen.com>
2024-02-22 19:15:54 -0700

    fix: imports and mock generation

--- resources/cloudformation_mock_test.go
+++ resources/cloudformation_mock_test.go
@@ -1,6 +1,4 @@
 //go:generate ../mocks/generate_mocks.sh cloudformation cloudformationiface
 package resources
 
-import _ "github.com/golang/mock/mockgen"
-
 // Note: empty on purpose, this file exist purely to generate mocks for the CloudFormation service
