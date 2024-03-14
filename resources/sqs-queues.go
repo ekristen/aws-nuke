741882934e2ff8022019686c82b282f7ec249708
Erik Kristensen <erik@erikkristensen.com>
2024-03-13 21:59:51 -0600

    feat(sqs-queues): add properties, tags, and mock tests

--- resources/sqs-queues.go
+++ resources/sqs-queues.go
@@ -2,6 +2,9 @@ package resources
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
+	"github.com/ekristen/libnuke/pkg/types"
+	"github.com/sirupsen/logrus"
 
 	"github.com/gotidy/ptr"
 
@@ -23,12 +26,19 @@ func init() {
 	})
 }
 
-type SQSQueueLister struct{}
+type SQSQueueLister struct {
+	mockSvc sqsiface.SQSAPI
+}
 
 func (l *SQSQueueLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
 	opts := o.(*nuke.ListerOpts)
 
-	svc := sqs.New(opts.Session)
+	var svc sqsiface.SQSAPI
+	if l.mockSvc != nil {
+		svc = l.mockSvc
+	} else {
+		svc = sqs.New(opts.Session)
+	}
 
 	params := &sqs.ListQueuesInput{}
 	resp, err := svc.ListQueues(params)
@@ -38,9 +48,21 @@ func (l *SQSQueueLister) List(_ context.Context, o interface{}) ([]resource.Reso
 
 	resources := make([]resource.Resource, 0)
 	for _, queue := range resp.QueueUrls {
+		var tags map[string]*string
+		queueTags, err := svc.ListQueueTags(&sqs.ListQueueTagsInput{
+			QueueUrl: queue,
+		})
+		if err != nil {
+			logrus.WithError(err).Error("unable to list queue tags")
+		}
+		if queueTags != nil {
+			tags = queueTags.Tags
+		}
+
 		resources = append(resources, &SQSQueue{
 			svc:      svc,
 			queueURL: queue,
+			tags:     tags,
 		})
 	}
 
@@ -48,8 +70,9 @@ func (l *SQSQueueLister) List(_ context.Context, o interface{}) ([]resource.Reso
 }
 
 type SQSQueue struct {
-	svc      *sqs.SQS
+	svc      sqsiface.SQSAPI
 	queueURL *string
+	tags     map[string]*string
 }
 
 func (f *SQSQueue) Remove(_ context.Context) error {
@@ -62,4 +85,16 @@ func (f *SQSQueue) Remove(_ context.Context) error {
 
 func (f *SQSQueue) String() string {
 	return ptr.ToString(f.queueURL)
+}
+
+func (f *SQSQueue) Properties() types.Properties {
+	properties := types.NewProperties()
+
+	properties.Set("QueueURL", f.queueURL)
+
+	for k, v := range f.tags {
+		properties.SetTag(ptr.String(k), v)
+	}
+
+	return properties
 }
