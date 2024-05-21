6953a970a10f66773fae7c8e6f8d92bc0bc905d5
Erik Kristensen <erik@erikkristensen.com>
2024-05-21 09:24:31 -0600

    feat: support config conformance pack removal

---
+++ resources/configservice-conformance-pack.go
@@ -0,0 +1,82 @@
+package resources
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/service/configservice"
+
+	"github.com/ekristen/libnuke/pkg/registry"
+	"github.com/ekristen/libnuke/pkg/resource"
+	"github.com/ekristen/libnuke/pkg/types"
+
+	"github.com/ekristen/aws-nuke/pkg/nuke"
+)
+
+const ConfigServiceConformancePackResource = "ConfigServiceConformancePack"
+
+func init() {
+	registry.Register(&registry.Registration{
+		Name:   ConfigServiceConformancePackResource,
+		Scope:  nuke.Account,
+		Lister: &ConfigServiceConformancePackLister{},
+	})
+}
+
+type ConfigServiceConformancePackLister struct{}
+
+func (l *ConfigServiceConformancePackLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
+	opts := o.(*nuke.ListerOpts)
+	svc := configservice.New(opts.Session)
+	var resources []resource.Resource
+
+	var nextToken *string
+
+	for {
+		res, err := svc.DescribeConformancePacks(&configservice.DescribeConformancePacksInput{
+			NextToken: nextToken,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, p := range res.ConformancePackDetails {
+			resources = append(resources, &ConfigServiceConformancePack{
+				svc:  svc,
+				id:   p.ConformancePackId,
+				name: p.ConformancePackName,
+			})
+		}
+
+		if res.NextToken == nil {
+			break
+		}
+
+		nextToken = res.NextToken
+	}
+
+	return resources, nil
+}
+
+type ConfigServiceConformancePack struct {
+	svc  *configservice.ConfigService
+	id   *string
+	name *string
+}
+
+func (r *ConfigServiceConformancePack) Remove(_ context.Context) error {
+	_, err := r.svc.DeleteConformancePack(&configservice.DeleteConformancePackInput{
+		ConformancePackName: r.name,
+	})
+	return err
+}
+
+func (r *ConfigServiceConformancePack) Properties() types.Properties {
+	props := types.NewProperties()
+	props.Set("ID", r.id)
+	props.Set("Name", r.name)
+	return props
+}
+
+func (r *ConfigServiceConformancePack) String() string {
+	return *r.id
+}
