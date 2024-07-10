3a3aae37a82f06f3ea97a23d7acec1839466edda
Erik Kristensen <erik@erikkristensen.com>
2024-07-10 15:14:15 -0600

    feat(iam-user-mfa-device): new resource for user specific mfa devices vs account devices

---
+++ resources/iam-user-mfa-device.go
@@ -0,0 +1,98 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gotidy/ptr"
+
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/iam/iamiface"
+
+	"github.com/ekristen/libnuke/pkg/registry"
+	"github.com/ekristen/libnuke/pkg/resource"
+	"github.com/ekristen/libnuke/pkg/types"
+
+	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
+)
+
+const IAMUserMFADeviceResource = "IAMUserMFADevice"
+
+func init() {
+	registry.Register(&registry.Registration{
+		Name:   IAMUserMFADeviceResource,
+		Scope:  nuke.Account,
+		Lister: &IAMUserMFADeviceLister{},
+	})
+}
+
+type IAMUserMFADeviceLister struct {
+	mockSvc iamiface.IAMAPI
+}
+
+func (l *IAMUserMFADeviceLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
+	opts := o.(*nuke.ListerOpts)
+	var resources []resource.Resource
+
+	var svc iamiface.IAMAPI
+	if l.mockSvc != nil {
+		svc = l.mockSvc
+	} else {
+		svc = iam.New(opts.Session)
+	}
+
+	allUsers, err := ListIAMUsers(svc)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, user := range allUsers {
+		res, listErr := svc.ListMFADevices(&iam.ListMFADevicesInput{
+			UserName: user.UserName,
+		})
+		if listErr != nil {
+			return nil, listErr
+		}
+
+		for _, p := range res.MFADevices {
+			nameParts := strings.Split(*p.SerialNumber, "/")
+			name := nameParts[len(nameParts)-1]
+
+			resources = append(resources, &IAMUserMFADevice{
+				svc:          svc,
+				Name:         ptr.String(name),
+				UserName:     p.UserName,
+				SerialNumber: p.SerialNumber,
+				EnableDate:   p.EnableDate,
+			})
+		}
+	}
+
+	return resources, nil
+}
+
+type IAMUserMFADevice struct {
+	svc          iamiface.IAMAPI
+	Name         *string
+	UserName     *string
+	SerialNumber *string
+	EnableDate   *time.Time
+}
+
+func (r *IAMUserMFADevice) Remove(_ context.Context) error {
+	_, err := r.svc.DeactivateMFADevice(&iam.DeactivateMFADeviceInput{
+		UserName:     r.UserName,
+		SerialNumber: r.SerialNumber,
+	})
+	return err
+}
+
+func (r *IAMUserMFADevice) Properties() types.Properties {
+	return types.NewPropertiesFromStruct(r)
+}
+
+func (r *IAMUserMFADevice) String() string {
+	return fmt.Sprintf("%s -> %s", *r.UserName, *r.Name)
+}
---
+++ resources/iam-user-mfa-device_mock_test.go
@@ -0,0 +1,83 @@
+package resources
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/gotidy/ptr"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+
+	"github.com/ekristen/aws-nuke/v3/mocks/mock_iamiface"
+	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
+)
+
+func Test_Mock_IAMUserMFADevice_List(t *testing.T) {
+	a := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockIAM := mock_iamiface.NewMockIAMAPI(ctrl)
+
+	mockIAM.EXPECT().ListUsersPages(gomock.Any(), gomock.Any()).
+		Do(func(_ *iam.ListUsersInput, fn func(page *iam.ListUsersOutput, lastPage bool) bool) {
+			fn(&iam.ListUsersOutput{
+				Users: []*iam.User{
+					{
+						UserName: ptr.String("foo"),
+					},
+				},
+			}, true)
+		}).Return(nil)
+
+	mockIAM.EXPECT().ListMFADevices(&iam.ListMFADevicesInput{
+		UserName: ptr.String("foo"),
+	}).Return(&iam.ListMFADevicesOutput{
+		MFADevices: []*iam.MFADevice{
+			{
+				UserName:     ptr.String("foo"),
+				SerialNumber: ptr.String("bar"),
+				EnableDate:   ptr.Time(time.Now()),
+			},
+		},
+	}, nil)
+
+	lister := IAMUserMFADeviceLister{
+		mockSvc: mockIAM,
+	}
+
+	resources, err := lister.List(context.TODO(), &nuke.ListerOpts{
+		Region: &nuke.Region{
+			Name: "us-east-2",
+		},
+		Session: session.Must(session.NewSession()),
+	})
+	a.NoError(err)
+	a.Len(resources, 1)
+}
+
+func Test_Mock_IAMUserMFADevice_Remove(t *testing.T) {
+	a := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockIAM := mock_iamiface.NewMockIAMAPI(ctrl)
+
+	mockIAM.EXPECT().DeactivateMFADevice(&iam.DeactivateMFADeviceInput{
+		UserName:     ptr.String("foo"),
+		SerialNumber: ptr.String("bar"),
+	})
+
+	resource := &IAMUserMFADevice{
+		svc:          mockIAM,
+		UserName:     ptr.String("foo"),
+		SerialNumber: ptr.String("bar"),
+	}
+
+	err := resource.Remove(context.TODO())
+	a.NoError(err)
+}
