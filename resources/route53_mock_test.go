fd4b87db3ce4f03cf645d79995f2bad453a9326a
Erik Kristensen <erik@erikkristensen.com>
2024-09-11 12:04:16 -0600

    test(route53): adding mocks for testing route53resolver

--- resources/route53_mock_test.go
+++ resources/route53_mock_test.go
@@ -1,4 +1,5 @@
 //go:generate ../mocks/generate_mocks.sh route53 route53iface
+//go:generate ../mocks/generate_mocks.sh route53resolver route53resolveriface
 package resources
 
 // Note: empty on purpose, this file exist purely to generate mocks for the Route53 service
