aa5374ec5febc5dd4c203e18e38ff799be56257b
Erik Kristensen <erik@erikkristensen.com>
2024-12-02 16:31:51 -0700

    refactor(elbv2-alb): standardize and property descriptions for docs

---
+++ resources/elbv2-alb_test.go
@@ -0,0 +1,36 @@
+package resources
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gotidy/ptr"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+)
+
+func Test_ELBv2LoadBalancer_Properties(t *testing.T) {
+	a := assert.New(t)
+	now := time.Now()
+
+	resource := ELBv2LoadBalancer{
+		Name:        ptr.String("foobar-name"),
+		ARN:         ptr.String("foobar-arn"),
+		CreatedTime: ptr.Time(now),
+		Tags: []*elbv2.Tag{
+			{
+				Key:   aws.String("Name"),
+				Value: aws.String("foobar-name"),
+			},
+		},
+	}
+
+	props := resource.Properties()
+
+	a.Equal("foobar-name", props.Get("Name"))
+	a.Equal("foobar-arn", props.Get("ARN"))
+	a.Equal(now.Format(time.RFC3339), props.Get("CreatedTime"))
+	a.Equal("foobar-name", props.Get("tag:Name"))
+}
