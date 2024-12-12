33df674ff43767e1adeab6d3e9e078d93b2c7825
Erik Kristensen <erik@erikkristensen.com>
2024-12-12 12:38:07 -0700

    fix(ec2-tgw): filter out gateways not owned by account

---
+++ resources/ec2-tgw_test.go
@@ -0,0 +1,28 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/gotidy/ptr"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func Test_EC2TGW_Properties(t *testing.T) {
+	tgw := &EC2TGW{
+		ID:      ptr.String("tgw-1234567890abcdef0"),
+		OwnerID: ptr.String("123456789012"),
+		Tags: []*ec2.Tag{
+			{
+				Key:   ptr.String("TestTag"),
+				Value: ptr.String("test-tgw"),
+			},
+		},
+	}
+
+	assert.Equal(t, "tgw-1234567890abcdef0", tgw.Properties().Get("ID"))
+	assert.Equal(t, "123456789012", tgw.Properties().Get("OwnerId"))
+	assert.Equal(t, "test-tgw", tgw.Properties().Get("tag:TestTag"))
+	assert.Equal(t, "tgw-1234567890abcdef0", tgw.String())
+}
