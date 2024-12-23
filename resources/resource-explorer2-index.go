aa8a6ffdec287f996b5a4dc7c12d1d3a24fb2865
Erik Kristensen <erik@erikkristensen.com>
2024-12-23 12:17:15 -0700

    feat(resource-explorer2-index): add properties

--- resources/resource-explorer2-index.go
+++ resources/resource-explorer2-index.go
@@ -2,11 +2,15 @@ package resources
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/resourceexplorer2"
-	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
+
 	"github.com/ekristen/libnuke/pkg/registry"
 	"github.com/ekristen/libnuke/pkg/resource"
+	"github.com/ekristen/libnuke/pkg/types"
+
+	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
 )
 
 const ResourceExplorer2IndexResource = "ResourceExplorer2Index"
@@ -21,11 +25,6 @@ func init() {
 }
 
 type ResourceExplorer2IndexLister struct{}
-
-type ResourceExplorer2Index struct {
-	svc      *resourceexplorer2.ResourceExplorer2
-	indexArn *string
-}
 
 func (l *ResourceExplorer2IndexLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
 	opts := o.(*nuke.ListerOpts)
@@ -45,8 +44,9 @@ func (l *ResourceExplorer2IndexLister) List(_ context.Context, o interface{}) ([
 
 		for _, index := range output.Indexes {
 			resources = append(resources, &ResourceExplorer2Index{
-				svc:      svc,
-				indexArn: index.Arn,
+				svc:  svc,
+				ARN:  index.Arn,
+				Type: index.Type,
 			})
 		}
 
@@ -60,14 +60,24 @@ func (l *ResourceExplorer2IndexLister) List(_ context.Context, o interface{}) ([
 	return resources, nil
 }
 
+type ResourceExplorer2Index struct {
+	svc  *resourceexplorer2.ResourceExplorer2
+	ARN  *string
+	Type *string
+}
+
 func (r *ResourceExplorer2Index) Remove(_ context.Context) error {
 	_, err := r.svc.DeleteIndex(&resourceexplorer2.DeleteIndexInput{
-		Arn: r.indexArn,
+		Arn: r.ARN,
 	})
 
 	return err
 }
 
 func (r *ResourceExplorer2Index) String() string {
-	return *r.indexArn
+	return *r.ARN
+}
+
+func (r *ResourceExplorer2Index) Properties() types.Properties {
+	return types.NewPropertiesFromStruct(r)
 }
