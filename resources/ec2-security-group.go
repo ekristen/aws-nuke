1fe55249cd31d8c3bfe11af46b9473f907dcfdf1
Erik Kristensen <erik@erikkristensen.com>
2025-01-30 17:03:52 -0700

    feat(ec2-security-group): filter shared resource

--- resources/ec2-security-group.go
+++ resources/ec2-security-group.go
@@ -2,17 +2,18 @@ package resources
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/gotidy/ptr"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
-
 	"github.com/ekristen/libnuke/pkg/registry"
 	"github.com/ekristen/libnuke/pkg/resource"
 	"github.com/ekristen/libnuke/pkg/types"
+
+	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
 )
 
 const EC2SecurityGroupResource = "EC2SecurityGroup"
@@ -31,13 +32,15 @@ func init() {
 }
 
 type EC2SecurityGroup struct {
-	svc     *ec2.EC2
-	group   *ec2.SecurityGroup
-	ID      *string
-	Name    *string
-	OwnerID *string
-	ingress []*ec2.IpPermission
-	egress  []*ec2.IpPermission
+	svc       *ec2.EC2
+	accountID *string
+	ingress   []*ec2.IpPermission
+	egress    []*ec2.IpPermission
+
+	ID      *string `description:"The ID of the security group."`
+	Name    *string `description:"The name of the security group."`
+	OwnerID *string `description:"The ID of the AWS account that owns the security group."`
+	Tags    []*ec2.Tag
 }
 
 type EC2SecurityGroupLister struct{}
@@ -53,13 +56,14 @@ func (l *EC2SecurityGroupLister) List(_ context.Context, o interface{}) ([]resou
 		func(page *ec2.DescribeSecurityGroupsOutput, lastPage bool) bool {
 			for _, group := range page.SecurityGroups {
 				resources = append(resources, &EC2SecurityGroup{
-					svc:     svc,
-					group:   group,
-					ID:      group.GroupId,
-					Name:    group.GroupName,
-					OwnerID: group.OwnerId,
-					ingress: group.IpPermissions,
-					egress:  group.IpPermissionsEgress,
+					svc:       svc,
+					accountID: opts.AccountID,
+					ingress:   group.IpPermissions,
+					egress:    group.IpPermissionsEgress,
+					ID:        group.GroupId,
+					Name:      group.GroupName,
+					OwnerID:   group.OwnerId,
+					Tags:      group.Tags,
 				})
 			}
 			return !lastPage
@@ -73,6 +77,10 @@ func (l *EC2SecurityGroupLister) List(_ context.Context, o interface{}) ([]resou
 func (r *EC2SecurityGroup) Filter() error {
 	if ptr.ToString(r.Name) == "default" {
 		return fmt.Errorf("cannot delete group 'default'")
+	}
+
+	if ptr.ToString(r.OwnerID) != ptr.ToString(r.accountID) {
+		return errors.New("not owned by account, likely shared")
 	}
 
 	return nil
@@ -110,13 +118,7 @@ func (r *EC2SecurityGroup) Remove(_ context.Context) error {
 }
 
 func (r *EC2SecurityGroup) Properties() types.Properties {
-	properties := types.NewProperties()
-	for _, tagValue := range r.group.Tags {
-		properties.SetTag(tagValue.Key, tagValue.Value)
-	}
-	properties.Set("Name", r.Name)
-	properties.Set("OwnerID", r.OwnerID)
-	return properties
+	return types.NewPropertiesFromStruct(r)
 }
 
 func (r *EC2SecurityGroup) String() string {
