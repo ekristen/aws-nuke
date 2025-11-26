b05ff00a9e3bffbb84fbed960fd1666d88285e44
Erik Kristensen <erik@erikkristensen.com>
2025-11-26 10:37:41 -0700

    test(bedrock-agentcore): fix oauth2 cred provider tests

--- resources/bedrockagentcorecontrol-oauth2credentialprovider_mock_test.go
+++ resources/bedrockagentcorecontrol-oauth2credentialprovider_mock_test.go
@@ -15,18 +15,16 @@ func Test_BedrockAgentCoreOauth2CredentialProvider_Properties(t *testing.T) {
 	updatedTime := time.Now()
 
 	resource := BedrockAgentCoreOauth2CredentialProvider{
-		Name:                     ptr.String("test-oauth2-provider"),
-		CredentialProviderArn:    ptr.String("arn:aws:bedrock:us-east-1:123456789012:oauth2-credential-provider/test"),
-		CredentialProviderVendor: "OKTA",
-		CreatedTime:              &createdTime,
-		LastUpdatedTime:          &updatedTime,
+		Name:            ptr.String("test-oauth2-provider"),
+		Vendor:          "OKTA",
+		CreatedTime:     &createdTime,
+		LastUpdatedTime: &updatedTime,
 	}
 
 	props := resource.Properties()
 
 	a.Equal("test-oauth2-provider", props.Get("Name"))
-	a.Equal("arn:aws:bedrock:us-east-1:123456789012:oauth2-credential-provider/test", props.Get("CredentialProviderArn"))
-	a.Equal("OKTA", props.Get("CredentialProviderVendor"))
+	a.Equal("OKTA", props.Get("Vendor"))
 	a.Equal(createdTime.Format(time.RFC3339), props.Get("CreatedTime"))
 	a.Equal(updatedTime.Format(time.RFC3339), props.Get("LastUpdatedTime"))
 }
