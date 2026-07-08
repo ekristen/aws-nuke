2151a0118b53ad1f4147c94d464fdc04be27df12
egrama <egrama@gmail.com>
2026-07-08 14:59:18 +0300

    feat(kinesis-stream): migrate to sdk v2 and add tag support
    Adds tag fetching via ListTagsForStream and exposes Name/tag:<key>
    as filterable properties, so streams can be kept/nuked by tag.
    Migrates the resource from aws-sdk-go v1 to v2 in the process.

--- resources/kinesis-streams.go
+++ resources/kinesis-streams.go
@@ -3,11 +3,12 @@ package resources
 import (
 	"context"
 
-	"github.com/aws/aws-sdk-go/aws"             //nolint:staticcheck
-	"github.com/aws/aws-sdk-go/service/kinesis" //nolint:staticcheck
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis"
 
 	"github.com/ekristen/libnuke/pkg/registry"
 	"github.com/ekristen/libnuke/pkg/resource"
+	"github.com/ekristen/libnuke/pkg/types"
 
 	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
 )
@@ -23,35 +24,63 @@ func init() {
 	})
 }
 
-type KinesisStreamLister struct{}
+type KinesisStreamAPI interface {
+	ListStreams(ctx context.Context, params *kinesis.ListStreamsInput,
+		optFns ...func(*kinesis.Options)) (*kinesis.ListStreamsOutput, error)
+	ListTagsForStream(ctx context.Context, params *kinesis.ListTagsForStreamInput,
+		optFns ...func(*kinesis.Options)) (*kinesis.ListTagsForStreamOutput, error)
+	DeleteStream(ctx context.Context, params *kinesis.DeleteStreamInput,
+		optFns ...func(*kinesis.Options)) (*kinesis.DeleteStreamOutput, error)
+}
 
-func (l *KinesisStreamLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
+type KinesisStreamLister struct {
+	svc KinesisStreamAPI
+}
+
+func (l *KinesisStreamLister) List(ctx context.Context, o interface{}) ([]resource.Resource, error) {
 	opts := o.(*nuke.ListerOpts)
 
-	svc := kinesis.New(opts.Session)
+	if l.svc == nil {
+		l.svc = kinesis.NewFromConfig(*opts.Config)
+	}
 
 	resources := make([]resource.Resource, 0)
 	var lastStreamName *string
 
 	params := &kinesis.ListStreamsInput{
-		Limit: aws.Int64(25),
+		Limit: aws.Int32(25),
 	}
 
 	for {
-		output, err := svc.ListStreams(params)
+		output, err := l.svc.ListStreams(ctx, params)
 		if err != nil {
 			return nil, err
 		}
 
 		for _, streamName := range output.StreamNames {
-			resources = append(resources, &KinesisStream{
-				svc:        svc,
-				streamName: streamName,
+			name := streamName
+
+			tags := make(map[string]string)
+			tagsResp, err := l.svc.ListTagsForStream(ctx, &kinesis.ListTagsForStreamInput{
+				StreamName: &name,
 			})
-			lastStreamName = streamName
+			if err != nil {
+				opts.Logger.Warnf("unable to fetch tags for kinesis stream: %s", name)
+			} else {
+				for _, tag := range tagsResp.Tags {
+					tags[*tag.Key] = *tag.Value
+				}
+			}
+
+			resources = append(resources, &KinesisStream{
+				svc:  l.svc,
+				Name: &name,
+				Tags: tags,
+			})
+			lastStreamName = &name
 		}
 
-		if !aws.BoolValue(output.HasMoreStreams) {
+		if output.HasMoreStreams == nil || !*output.HasMoreStreams {
 			break
 		}
 
@@ -62,18 +91,23 @@ func (l *KinesisStreamLister) List(_ context.Context, o interface{}) ([]resource
 }
 
 type KinesisStream struct {
-	svc        *kinesis.Kinesis
-	streamName *string
+	svc  KinesisStreamAPI
+	Name *string           `description:"The name of the Kinesis stream"`
+	Tags map[string]string `description:"The tags associated with the Kinesis stream"`
 }
 
-func (f *KinesisStream) Remove(_ context.Context) error {
-	_, err := f.svc.DeleteStream(&kinesis.DeleteStreamInput{
-		StreamName: f.streamName,
+func (f *KinesisStream) Remove(ctx context.Context) error {
+	_, err := f.svc.DeleteStream(ctx, &kinesis.DeleteStreamInput{
+		StreamName: f.Name,
 	})
 
 	return err
 }
 
+func (f *KinesisStream) Properties() types.Properties {
+	return types.NewPropertiesFromStruct(f)
+}
+
 func (f *KinesisStream) String() string {
-	return *f.streamName
+	return *f.Name
 }
